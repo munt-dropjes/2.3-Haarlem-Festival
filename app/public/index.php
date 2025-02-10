285870a46c93cc618524fdbe81e771126943d31c
Daniel Zwart <Dtzwart@gmail.com>
2025-02-10 18:55:22 +0100

    All Base classes + folder hierarchy

---
+++ app/public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+use Bramus\Router\Router;
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$router = new Router();
+$router->setNamespace('\Controllers');
+
+// for more info visit: https://github.com/bramus/router
+// route setup is like this:
+// $router->get('/YOURPATH', 'YOURCONTROLLER@YOURFUNCTION');
+
+// Add your routes here:
+// default route
+    //home
+    $router->get('/', 'HomeController@index');
+    $router->get('/home', 'HomeController@index');
+
+// Run the router
+$router->run();
