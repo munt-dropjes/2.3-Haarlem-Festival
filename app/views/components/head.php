dd9b65d483c5e42e944d110d00da1d0c1781cae1
Natfaji_A <modemoon2003@hotmail.com>
2025-02-13 15:07:33 +0100

    Add head, header, footer, and sidebar functionality with associated styles and icons

---
+++ app/views/components/head.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Haarlem Festival</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="/css/style.css">
+
+	<!-- JS -->
+	<script src="/js/global.js"></script>
+
+	<!-- Favicon -->
+	<link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg">
+	<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32.png">
+	<link rel="icon" type="image/png" sizes="48x48" href="/assets/favicon/favicon-48.png">
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+	<script src="/js/bootstrap.bundle.min.js" defer></script>
+
+	<!-- FontAwesom -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+		integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+		crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<!-- Google links -->
+	<link href="https://fonts.googleapis.com/css2?family=Monoton&display=swap" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
