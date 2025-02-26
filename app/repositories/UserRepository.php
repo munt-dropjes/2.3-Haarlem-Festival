e9b6f10a66e4e834eb330444f0f404c7e3bcfabd
Daniel Zwart <Dtzwart@gmail.com>
2025-02-26 18:32:00 +0100

    error handling

--- app/repositories/UserRepository.php
+++ app/repositories/UserRepository.php
@@ -23,7 +23,7 @@ class UserRepository extends BaseRepository{
             return $this->getUser($user['email']);
         }
         catch(Exception $e){
-            throw new Exception($e->getMessage());
+            throw new Exception("Error code: " . $e->getCode() . " -  Something went wrong trying to create user: " . $user->email);
         }
     }
 
@@ -50,35 +50,76 @@ class UserRepository extends BaseRepository{
             return $users;
         }
         catch(Exception $e){
-            throw new Exception($e->getMessage());
+            throw new Exception("Error code: " . $e->getCode() . " -  Something went wrong trying to get all users");
         }
     }
 
     public function getUser($user): ?User
     {
-        $email = $user->getEmail();
-        $stmt = $this->connection->prepare("SELECT * FROM Users WHERE Email = :email");
-        $stmt->bindParam(':email', $email, \PDO::PARAM_STR);
-        $stmt->execute();
-        $stmt->setFetchMode(\PDO::FETCH_CLASS, 'Models\User');
-        $fetchedUser = $stmt->fetch();
-        return $fetchedUser ?: null;
+        try{
+            $email = $user->getEmail();
+            $stmt = $this->connection->prepare("SELECT * FROM Users WHERE Email = :email");
+            $stmt->bindParam(':email', $email, \PDO::PARAM_STR);
+            $stmt->execute();
+            $stmt->setFetchMode(\PDO::FETCH_CLASS, 'Models\User');
+            $fetchedUser = $stmt->fetch();
+            return $fetchedUser ?: null;
+        }
+        catch(Exception $e){
+            throw new Exception("Error code: " . $e->getCode() . " -  Something went wrong trying to get user: " . $user->email);
+        }
     }
 
     public function checkEmail($user): bool
     {
-        $stmt = $this->connection->prepare("SELECT * FROM Users WHERE email = ?");
-        $stmt->execute([$user->getEmail()]);
-        $user = $stmt->fetch(\PDO::FETCH_ASSOC);
-        if ($user) {
-                return true;
-        } else {
-                return false;
+        try{
+            $stmt = $this->connection->prepare("SELECT * FROM Users WHERE email = ?");
+            $stmt->execute([$user->getEmail()]);
+            $user = $stmt->fetch(\PDO::FETCH_ASSOC);
+            if ($user) {
+                    return true;
+            } else {
+                    return false;
+            }
+        } 
+        catch(Exception $e){
+            throw new Exception("Error code: " . $e->getCode() . " -  Something went wrong trying to check email: " . $user->email);
         }
     }
 
     // ~~Update~~
+    public function updateUser($user) : User {
+        try{
+            $sql = "UPDATE Users 
+                    SET role = ?, name = ?, email = ?, password = ?, phone = ?, country = ?
+                    WHERE email = ?";
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute([
+                $user->getRole(),
+                $user->getName(),
+                $user->getEmail(),
+                $user->getPassword(),
+                $user->getPhone(),
+                $user->getCountry(),
+                $user->getEmail()
+            ]);
+            return $this->getUser($user);
+        }
+        catch(Exception $e){
+            throw new Exception("Error code: " . $e->getCode() . " -  Something went wrong trying to update user: " . $user->email);
+        }
+    }
 
     // ~~Delete~~
+    public function deleteUser($email) : void {
+        try{
+            $sql = "DELETE FROM Users WHERE email = ?";
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute([$email]);
+        }
+        catch(Exception $e){
+            throw new Exception("Error code: " . $e->getCode() . " -  Something went wrong trying to delete user: " . $email);
+        }
+    }
 
 }
