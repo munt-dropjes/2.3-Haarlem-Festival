8fa875451d0716bd53e01e20eb6bdfa460e50911
Toast3347 <144022532+Toast3347@users.noreply.github.com>
2025-03-04 19:55:45 +0100

    Create StrollRepository.php

---
+++ app/repositories/StrollRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Repositories;
+
+use Models\StrollEvent;
+
+
+class StrollRepository extends BaseRepository
+{
+    public function create(StrollEvent $event)
+    {
+
+    }
+
+    public function getAll(StrollEvent $event)
+    {
+
+    }
+
+    public function update(StrollEvent $event)
+    {
+
+    }
+
+    public function delete(StrollEvent $event)
+    {
+        
+    }
+}
