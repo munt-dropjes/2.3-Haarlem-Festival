2290e22074a1c689d14b3d592d496b0586edabee
Daniel Zwart <Dtzwart@gmail.com>
2025-03-07 13:46:01 +0100

    Update ForgotPasswordController.php

--- app/controllers/ForgotPasswordController.php
+++ app/controllers/ForgotPasswordController.php
@@ -19,18 +19,23 @@ class ForgotPasswordController extends Controller {
             $this->mailerService = new MailerService();
         }
     public function index() {
-        if ($_SERVER['REQUEST_METHOD'] = 'POST') {
-            $email = htmlspecialchars(strtolower($_POST['email']));
-            $user = $this->userService->getUserByEmail($email);
-            if ($user) {
-                $this->forgotPasswordService->createResetToken($user);
-                $this->mailerService->sendMail($user->getEmail(), $user->getName(), 'Password Reset', 'Please click the following link to reset your password: ' . $_SERVER['HTTP_HOST'] . '/resetpassword/' . $user->getResetToken());
-                $this->view('forgotPassword/success');
-            } else {
-                $this->view('forgotPassword/index', ['error' => 'No user found with that email address']);
-            }
+        $email = htmlspecialchars(strtolower($_POST['email']));
+        $user = $this->userService->getUserByEmail($email);
+        if ($user) {
+            $this->forgotPasswordService->createResetToken($user);
+            $this->mailerService->sendMail($user->getEmail(), $user->getName(), 'Password Reset', 'Please click the following link to reset your password: ' . $_SERVER['HTTP_HOST'] . '/resetpassword/' . $user->getResetToken());
+            $this->view('forgotPassword/success');
         } else {
-            $this->view('forgotPassword/index');
+            $this->view('forgotPassword/index', ['error' => 'No user found with that email address']);
         }
     }  
+
+    public function reset($resetToken) {
+        
+        if ($user) {
+            $this->view('forgotPassword/reset', ['resetToken' => $resetToken]);
+        } else {
+            $this->view('forgotPassword/reset', ['error' => 'Invalid reset token']);
+        }
+    }
 }
