c15031c3d9b5b95584b9821199ddceb3380c5ab0
JaydenVanKlooster <144449745+JaydenVanKlooster@users.noreply.github.com>
2025-03-13 16:51:37 +0100

    wow

--- app/views/jazz/artistDetail.php
+++ app/views/jazz/artistDetail.php
@@ -10,15 +10,55 @@
             </h1>
         </div>
     </div>
-    <img src="/images/jazz/<?= htmlspecialchars($artist['image']) ?>" alt="<?= htmlspecialchars($artist['name']) ?>"
+    <?php $fotocounter=0; ?>
+    <?php $fotocounter++; ?>
+    <img  src="/images/jazz/detail/<?= strtolower(str_replace(' ', '', $artist['name'])) ?>/<?=$fotocounter?>.png"
         class="img-fluid mb-4" style="width: 100vw; max-width: 100%; display: block;">
     <div class="container" style="display: flex; align-items: center;">
         <div style="width: 50%; margin-right: 20px;">
             <h2>A bit about <?= htmlspecialchars($artist['name']) ?></h2>
             <p><?= htmlspecialchars($artist['description']) ?></p>
         </div>
-        <img src="/images/jazz/detail/<?= strtolower(str_replace(' ', '', $artist['name'])) ?>/1.png" alt="<?= htmlspecialchars($artist['name']) ?>"
-            class="img-fluid mb-3" style="max-width: 40%;">
+        <?php $fotocounter++; ?>
+        <img src="/images/jazz/detail/<?= strtolower(str_replace(' ', '', $artist['name'])) ?>/<?=$fotocounter?>.png"
+            alt="<?= htmlspecialchars($artist['name']) ?>" class="img-fluid mb-3" style="max-width: 40%;">
     </div>
+
+
+        <!-- Artist Photo Container -->
+        <div class="artistfoto-container" style="max-width: 50%; display: flex; gap: 10px;">
+            <div class="artist-photo"style="height: 300;">
+            <?php for ($i = 1; $i <= 2; $i++) { $fotocounter++; ?>
+                <img src="/images/jazz/detail/<?= strtolower(str_replace(' ', '', $artist['name'])) ?>/<?=$fotocounter?>.png"
+                    alt="<?= htmlspecialchars($artist['name']) ?>" class="img-fluid rounded">
+                    <?php } ?>
+            </div>
+        </div>
+        <!-- Player Container -->
+        <div id="player" style="width: 50%; float: right;">
+            <?php
+            for ($i = 1; $i <= 3; $i++) {
+                if (isset($artist["Song{$i}Link"])) {
+                    $link = htmlspecialchars($artist["Song{$i}Link"]);
+
+                    // Spotify embed
+                    if (strpos($link, 'spotify.com') !== false) {
+                        $embedUrl = str_replace('open.spotify.com', 'embed.spotify.com', $link);
+                        echo "<iframe src='$embedUrl' width='100%' height='166' frameborder='0' allowtransparency='true' allow='encrypted-media'></iframe>";
+                    }
+                    // SoundCloud embed
+                    elseif (strpos($link, 'soundcloud.com') !== false) {
+                        echo "<iframe width='100%' height='166' scrolling='no' frameborder='no' allow='autoplay' 
+                src='https://w.soundcloud.com/player/?url=$link'></iframe>";
+                    } else {
+                        echo "<p>Ongeldige link. Gebruik een geldige Spotify- of SoundCloud-link.</p>";
+                    }
+                } else {
+                    echo "<p>Geen muzieklink beschikbaar.</p>";
+                }
+            }
+            ?>
+        </div>
+
 
 </main>
