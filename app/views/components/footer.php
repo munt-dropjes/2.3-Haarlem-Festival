3f75bcb4ffe3b284762d90f047ca48a5184d4cd7
Natfaji_A <modemoon2003@hotmail.com>
2025-03-14 14:59:47 +0100

    make footer responsive

--- app/views/components/footer.php
+++ app/views/components/footer.php
@@ -1,43 +1,45 @@
 <footer class="mt-5">
 	<img src="/assets/icons/haarlem.svg" alt="">
 
-	<div class="d-flex p-3 pt-0 bg-black">
-		<ul class="col-8 nav list-unstyled d-flex align-self-end gap-4">
-			<li>
-				<a class="text-body-secondary" href="#">
-					<img class="footer-icon" src="/assets/icons/facebook.svg" alt="">
-				</a>
-			</li>
-			<li>
-				<a class="text-body-secondary" href="#">
-					<img class="footer-icon" src="/assets/icons/instagram.svg" alt="">
-				</a>
-			</li>
-			<li>
-				<a class="text-body-secondary" href="#">
-					<img class="footer-icon" src="/assets/icons/whatsapp.svg" alt="">
-				</a>
-			</li>
-			<li>
-				<a class="text-body-secondary" href="#">
-					<img class="footer-icon" src="/assets/icons/x.svg" alt="">
-				</a>
-			</li>
-			<li>
-				<a class="text-body-secondary" href="#">
-					<img class="footer-icon" src="/assets/icons/youtube.svg" alt="">
-				</a>
-			</li>
-		</ul>
+	<div class="bg-black">
+		<div class="container-xxl d-flex flex-column-reverse gap-4 flex-sm-row p-3 pt-0">
+			<ul class="flex-grow-1 nav list-unstyled d-flex align-self-center align-self-sm-end gap-4">
+				<li>
+					<a class="text-light fs-1" href="#">
+						<i class="fa-brands fa-facebook"></i>
+					</a>
+				</li>
+				<li>
+					<a class="text-light fs-1" href="#">
+						<i class="fa-brands fa-instagram"></i>
+					</a>
+				</li>
+				<li>
+					<a class="text-light fs-1" href="#">
+						<i class="fa-brands fa-whatsapp"></i>
+					</a>
+				</li>
+				<li>
+					<a class="text-light fs-1" href="#">
+						<i class="fa-brands fa-x-twitter"></i>
+					</a>
+				</li>
+				<li>
+					<a class="text-light fs-1" href="#">
+						<i class="fa-brands fa-youtube"></i>
+					</a>
+				</li>
+			</ul>
 
-		<form action="" method="post" class="col-4">
-			<div class="mb-4">
-				<label for="email" class="text-white mb-2">E-mail adres</label>
-				<input type="email" name="email" id="email" class="form-control" placeholder="submit e-mail address"
-					required>
-			</div>
-			<button type="submit" class="mt-2 py-2 px-5 CTA" name="submit">subscribe</button>
-		</form>
+			<form action="" method="post" class="col-12 col-sm-4">
+				<div class="mb-4">
+					<label for="email" class="text-white mb-2">E-mail adres</label>
+					<input type="email" name="email" id="email" class="form-control" placeholder="submit e-mail address"
+						required>
+				</div>
+				<button type="submit" class="mt-2 py-2 w-100 CTA" name="submit">subscribe</button>
+			</form>
+		</div>
 	</div>
 </footer>
 
