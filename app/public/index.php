714124d5c12c9c3edd42b62da6197b817dcd9827
Natfaji_A <modemoon2003@hotmail.com>
2025-03-25 00:36:45 +0100

    format indentations; add shopping cart routes and update quantity functionality

--- app/public/index.php
+++ app/public/index.php
@@ -4,7 +4,7 @@ use Bramus\Router\Router;
 
 require_once __DIR__ . '/../Models/User.php';
 if (session_status() == PHP_SESSION_NONE) {
-    session_start();
+	session_start();
 }
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -13,15 +13,15 @@ $router = new Router();
 $router->setNamespace('\Controllers');
 
 // for cms routes, we will check for authentication
-$router->before('GET|POST', '/cms/.*', function() {
-    if (str_contains($_SERVER['REQUEST_URI'], '/cms/login') || str_contains($_SERVER['REQUEST_URI'], '/cms/logout')) {
-        return;
-    }
-    
-    // if (!isset($_SESSION['user'])) {
-    //     header('Location: /login');
-    //     exit();
-    // }
+$router->before('GET|POST', '/cms/.*', function () {
+	if (str_contains($_SERVER['REQUEST_URI'], '/cms/login') || str_contains($_SERVER['REQUEST_URI'], '/cms/logout')) {
+		return;
+	}
+
+	// if (!isset($_SESSION['user'])) {
+	//     header('Location: /login');
+	//     exit();
+	// }
 });
 
 // for more info visit: https://github.com/bramus/router
@@ -30,35 +30,42 @@ $router->before('GET|POST', '/cms/.*', function() {
 
 // Add your routes here:
 // default route
-    //home
-    $router->get('/', 'HomeController@index');
-    $router->get('/home', 'HomeController@index');
+//home
+$router->get('/', 'HomeController@index');
+$router->get('/home', 'HomeController@index');
 
-    //everything account related
-    $router->get('/createaccount', 'createaccountController@index');
-    $router->post('/createaccount', 'createaccountController@create');
-    $router->get('/login', 'LoginController@index');
-    $router->post('/login', 'LoginController@login');
-    $router->get('/logout', 'LogOutController@index');
-    $router->get('/forgotpassword', 'ForgotPasswordController@index');
-    $router->post('/forgotpassword', 'ForgotPasswordController@index');
-    $router->get('/resetpassword/{email}/{resetToken}', 'ForgotPasswordController@reset');
-    $router->post('/resetpassword/{email}/{resetToken}', 'ForgotPasswordController@reset');
-    $router->get('/updateaccount', 'UpdateAccountController@index');
-    $router->post('/updateaccount', 'UpdateAccountController@updateAccount');
-	
-    //events
-    $router->get('/stroll', 'StrollController@index');
-    $router->get('/stroll/detail', 'StrollController@detail');
-	  $router->get('/dance', 'DanceController@index');
-	  $router->get('/dance/{artist}', 'DanceController@artist');
+//everything account related
+$router->get('/createaccount', 'createaccountController@index');
+$router->post('/createaccount', 'createaccountController@create');
+$router->get('/login', 'LoginController@index');
+$router->post('/login', 'LoginController@login');
+$router->get('/logout', 'LogOutController@index');
+$router->get('/forgotpassword', 'ForgotPasswordController@index');
+$router->post('/forgotpassword', 'ForgotPasswordController@index');
+$router->get('/resetpassword/{email}/{resetToken}', 'ForgotPasswordController@reset');
+$router->post('/resetpassword/{email}/{resetToken}', 'ForgotPasswordController@reset');
+$router->get('/updateaccount', 'UpdateAccountController@index');
+$router->post('/updateaccount', 'UpdateAccountController@updateAccount');
 
-    //cms
-    $router->get('/cms', 'CmsController@index');
-    $router->get('/cms/users', 'CmsUserController@index');
-    $router->post('/cms/users/create', 'CmsUserController@create');
-    $router->post('/cms/users/delete', 'CmsUserController@delete');
-    $router->post('/cms/users/edit', 'CmsUserController@update');
-    
+//events
+$router->get('/stroll', 'StrollController@index');
+$router->get('/stroll/detail', 'StrollController@detail');
+$router->get('/dance', 'DanceController@index');
+$router->get('/dance/{artist}', 'DanceController@artist');
+
+//cms
+$router->get('/cms', 'CmsController@index');
+$router->get('/cms/users', 'CmsUserController@index');
+$router->post('/cms/users/create', 'CmsUserController@create');
+$router->post('/cms/users/delete', 'CmsUserController@delete');
+$router->post('/cms/users/edit', 'CmsUserController@update');
+
+// shopping cart
+$router->get('/shopping-cart', 'ShoppingCartController@index');
+
+// shopping cart actions
+$router->get('/shopping-cart/update-quantity/{itemID}/{quantity}', 'ShoppingCartController@updateQuantity');
+$router->get('/shopping-cart/remove-item/{itemID}', 'ShoppingCartController@removeItem');
+
 // Run the router
 $router->run();
