08345e97617b3dc8895ed9e0b7662ac6898e2322
Toast3347 <144022532+Toast3347@users.noreply.github.com>
2025-03-24 15:25:32 +0100

    Update pdf service
    Made a basic template for the generation of an invoice or a ticket

--- app/services/pdfService.php
+++ app/services/pdfService.php
@@ -3,7 +3,7 @@
 	use Dompdf\Dompdf;
 
 	class PdfService{
-		public function generatePdf($html){
+		private function generatePDF($html){
 		
 			$dompdf = new Dompdf();
 			$dompdf->loadHtml($html);
@@ -11,6 +11,41 @@
 			$dompdf->render();
 
 			return $dompdf->output();
+		}
+
+		public function generateTicketPDF($ticket){
+			$html = '<!DOCTYPE html>
+			<html>
+			<head>
+				<title>Ticket</title>
+			</head>
+			<body>
+				<h1>Ticket</h1>
+				<p>Event: '.$ticket->getEvent().'</p>
+				<p>Price: '.$ticket->getPrice().'</p>
+				<p>Seat: '.$ticket->getSeat().'</p>
+			</body>
+			</html>';
+
+			return $this->generatePDF($html);
+		}
+
+
+		public function generateInvoicePDF($invoice){
+			$html = '<!DOCTYPE html>
+			<html>
+			<head>
+				<title>Invoice</title>
+			</head>
+			<body>
+				<h1>Invoice</h1>
+				<p>Event: '.$invoice->getEvent().'</p>
+				<p>Price: '.$invoice->getPrice().'</p>
+				<p>Seat: '.$invoice->getSeat().'</p>
+			</body>
+			</html>';
+
+			return $this->generatePDF($html);
 		}
 	} 
 	?>
