81e3548248fb0ab3f21979791d9132b1ae87d5ae
Daniel Zwart <Dtzwart@gmail.com>
2025-03-26 23:20:54 +0100

    fix

--- app/public/index.php
+++ app/public/index.php
@@ -61,26 +61,17 @@ $router->before('GET|POST', '/cms/.*', function() {
     $router->post('/cms/users/create', 'CmsUserController@create');
     $router->post('/cms/users/delete', 'CmsUserController@delete');
     $router->post('/cms/users/edit', 'CmsUserController@update');
-
-
-    //payment with stripe / shoppingcart routes
-    $router->get('/checkout', 'PaymentController@createSession');
-    $router->get('/checkout/complete', 'PaymentController@success');
-    $router->get('/checkout/cancel', 'PaymentController@cancel');
-    $router->post('/checkout/webhook', 'PaymentController@webhook');
-
     $router->get('/cms/events', 'CmsEventController@index');
     $router->post('/cms/events/create', 'CmsEventController@create');
     $router->post('/cms/events/delete', 'CmsEventController@delete');
     $router->post('/cms/events/edit', 'CmsEventController@update');
     $router->get('/cms/orders', 'CmsOrderController@index');
 
-    
-
-    //test remove before merging is to test the pdf generation and sending it through email
-    $router->get('/test', 'TestController@index');
-    $router->get('/download-ticket', 'TestController@downloadTicket');
-    $router->get('/download-invoice', 'TestController@downloadInvoice');
+    //payment with stripe / shoppingcart routes
+    $router->get('/checkout', 'PaymentController@createSession');
+    $router->get('/checkout/complete', 'PaymentController@success');
+    $router->get('/checkout/cancel', 'PaymentController@cancel');
+    $router->post('/checkout/webhook', 'PaymentController@webhook');    
 
 // Run the router
 $router->run();
