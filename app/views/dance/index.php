f1ee6e8291d85a620871bd6711af4d681157111a
Natfaji_A <modemoon2003@hotmail.com>
2025-04-09 12:50:37 +0200

    fix header navigation

--- app/views/dance/index.php
+++ app/views/dance/index.php
@@ -15,7 +15,7 @@
 						/** @var \Models\Artist $artist */ ?>
 						<a href="/dance/hardwell" class="swiper-slide swiper-slide-normal card overflow-hidden rounded-4">
 							<div class="image-container position-relative">
-								<img class="ArtistImage card-image-top" src="/images/<?= $artist->getImageName() ?>" alt="">
+								<img class="ArtistImage card-image-top" src="/images/dance/<?= $artist->getImageName() ?>" alt="">
 								<i class="text-center fw-bold d-none"><?= $artist->getName() ?></i>
 							</div>
 						</a>
@@ -36,7 +36,7 @@
 						/** @var \Models\Event $event */ ?>
 						<div class="swiper-slide swiper-slide-extended card overflow-hidden rounded-4">
 							<div class="image-container position-relative">
-								<img clang="EventImage card-img-top" src="/images/<?= $event->getImageName() ?>"
+								<img clang="EventImage card-img-top" src="/images/dance/<?= $event->getImageName() ?>"
 									alt="foto van <?= $event->getName() ?>">
 								<i class="text-center fw-bold d-none"><?= $event->getName() ?></i>
 							</div>
@@ -66,7 +66,7 @@
 						/** @var \Models\Event $event */ ?>
 						<div class="swiper-slide swiper-slide-extended card overflow-hidden rounded-4">
 							<div class="image-container position-relative">
-								<img clang="EventImage card-img-top" src="/images/<?= $event->getImageName() ?>"
+								<img clang="EventImage card-img-top" src="/images/dance/<?= $event->getImageName() ?>"
 									alt="foto van <?= $event->getName() ?>">
 								<i class="text-center fw-bold d-none"><?= $event->getName() ?></i>
 							</div>
@@ -96,7 +96,7 @@
 						/** @var \Models\Event $event */ ?>
 						<div class="swiper-slide swiper-slide-extended card overflow-hidden rounded-4">
 							<div class="image-container position-relative">
-								<img clang="EventImage card-img-top" src="/images/<?= $event->getImageName() ?>"
+								<img clang="EventImage card-img-top" src="/images/dance/<?= $event->getImageName() ?>"
 									alt="foto van <?= $event->getName() ?>">
 								<i class="text-center fw-bold d-none"><?= $event->getName() ?></i>
 							</div>
@@ -126,7 +126,7 @@
 						/** @var \Models\Event $event */ ?>
 						<div class="event-button-hover swiper-slide swiper-slide-normal card overflow-hidden rounded-4">
 							<div class="image-container position-relative">
-								<img clang="card-img-top" src="/images/<?= $event->getImageName() ?>" alt="All Access Pass">
+								<img clang="card-img-top" src="/images/dance/<?= $event->getImageName() ?>" alt="All Access Pass">
 								<i class="text-center fw-bold">
 									<span>
 										<?= date('l', strtotime($event->getDate())) ?>
